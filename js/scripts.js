947b7918e3ed46534d495c2952a42d9eadbf79e3
Alex Migdol and Shelby Clayton <shelby@>
2016-05-25 20:23:54 -0700

    add javascript to submit answers in the form

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-
-
+  $("#blanks").submit(function() {
+    var name = $("#name").val();
+    var genre = $("input:radio[name=genre]:checked").val();
+    var dob = $("#born").val();
+    var food = $("#food").val();
+  });
 });
